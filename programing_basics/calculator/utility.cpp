f3c70f263c0d703280d74a90491e85d8abd238b6
lumymx <lumymx00@gmail.com>
2023-03-25 22:56:59 +0300

    add utility

---
+++ programing_basics/calculator/utility.cpp
@@ -0,0 +1,45 @@
+#include "./utility.h"
+
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <cmath>
+
+std::string doubleToString(double number, int limit) {
+    std::stringstream ss;
+    ss.precision(limit);
+    ss << std::fixed << number;
+    std::string str = ss.str();
+
+    if (str.length() > limit) {
+        std::string::size_type dot_pos = str.find('.');
+        if (dot_pos == std::string::npos) {
+            dot_pos = str.length();
+        }
+        std::string::size_type e_pos = str.find('e');
+        if (e_pos != std::string::npos && e_pos > dot_pos) {
+            int exp = std::stoi(str.substr(e_pos+1));
+            if (exp >= 0 && exp < 3) {
+                str.erase(e_pos);
+            }
+        }
+        str.erase(limit);
+    }
+
+    return str;
+}
+
+std::string labelToFunction(std::string label){
+    std::string function = "";
+    if (label == "SIN")
+        function = "sin";
+    else if (label == "COS")
+        function = "cos";
+    else if (label == "TAN")
+        function = "tan";
+    else if (label == "COT")
+        function = "cot";
+    else if (label == "√")
+        function = "sqrt";
+    return function;
+}
---
+++ programing_basics/calculator/utility.h
@@ -0,0 +1,9 @@
+#ifndef UTILITY_H
+#define UTILITY_H
+
+#include <string>
+
+std::string doubleToString(double number, int limit);
+std::string labelToFunction(std::string label);
+
+#endif // UTILITY_H
